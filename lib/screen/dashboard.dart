8eca779fc73943a0664ef63c87ed7231de2c10e3
Vince Suinan <suinanlawrence8@gmail.com>
2025-02-05 17:01:33 +0800

    Update Dashboard

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -13,201 +13,267 @@ class DashboardtState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          // decoration: BoxDecoration(),
-          // color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.only(
-              top: 50.0,
-              left: 20.0,
-              right: 20.0,
-            ),
-            child: Column(
-              children: [
-                Container(
-                  // color: Colors.white,
+      body: ListView(
+        children: [
+          Container(
+            color: const Color.fromARGB(255, 101, 27, 219),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 8.0, left: 10.0, right: 10.0),
+              child: Container(
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: 80.0,
+                  ),
+                  Row(
                     children: [
-                      Row(
-                        children: [
-                          Text(
-                            "Welcome, ",
-                            style: GoogleFonts.poppins(
-                              fontSize: 25,
-                              color: const Color.fromARGB(255, 255, 255, 255),
-                            ),
-                          ),
-                          Text(
-                            "Dreams ",
-                            style: GoogleFonts.poppins(
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                                color: Theme.of(context).primaryColor),
-                          ),
-                        ],
+                      Text(
+                        "Welcome, ",
+                        style: GoogleFonts.poppins(
+                          fontSize: 25,
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                        ),
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => MyWelcomeView()));
-                        },
-                        child: Container(
-                          width: 45,
-                          height: 45,
-                          decoration: BoxDecoration(
-                            // color: Colors.white,
-                            borderRadius: BorderRadius.circular(50),
-                            border: Border.all(
-                              width: 3,
-                              color: Theme.of(context).primaryColor,
-                            ),
+                      Text(
+                        "Dreams ",
+                        style: GoogleFonts.poppins(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).primaryColor),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyWelcomeView()));
+                    },
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        // color: Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border.all(
+                          width: 3,
+                          color: Theme.of(context).primaryColor,
+                        ),
 
-                            image: DecorationImage(
-                              image: AssetImage("assets/images/logo2.png"),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/logo2.png"),
+                          fit: BoxFit.cover,
                         ),
-                      )
-                    ],
-                  ),
-                ),
-                Row(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          "HEADER ",
-                          style: GoogleFonts.poppins(
-                            fontSize: 25,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                        Text(
-                          "INFORMATION",
-                          style: GoogleFonts.poppins(
-                            fontSize: 25,
-                            color: Colors.white,
-                          ),
-                        )
-                      ],
+                      ),
                     ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(
-                        "News",
-                        style: GoogleFonts.poppins(
-                          fontSize: 15,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "Trending",
-                        style: GoogleFonts.poppins(
-                          fontSize: 15,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "Features",
-                        style: GoogleFonts.poppins(
-                          fontSize: 15,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "Schedule",
-                        style: GoogleFonts.poppins(
-                          fontSize: 15,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
                   ),
-                ),
-                // Column(
-                //   children: [
-                //     SizedBox(
-                //       height: 20.0,
-                //     ),
-                //     Image.asset(
-                //       "images/pattslogo.png",
-                //     ),
-                //     Text("Sample"),
-                //     Text("Sample"),
-                //     Text("Sample"),
-                //   ],
-                // ),
-                Container(
-                    width: double.infinity,
-                    height: 200,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (BuildContext context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                          child: Column(
-                            children: [
-                              SizedBox(
-                                height: 20.0,
-                              ),
-                              Image.asset(
-                                "images/pattslogo.png",
-                              ),
-                              SizedBox(
-                                height: 20.0,
-                              ),
-                              Image.asset(
-                                "images/pattslogo.png",
-                              ),
-                              SizedBox(
-                                height: 20.0,
-                              ),
-                              Image.asset(
-                                "images/pattslogo.png",
-                              ),
-                              SizedBox(
-                                height: 20.0,
-                              ),
-                              Image.asset(
-                                "images/pattslogo.png",
-                              ),
-                              SizedBox(
-                                height: 20.0,
-                              ),
-                              Image.asset(
-                                "images/pattslogo.png",
-                              ),
-                              SizedBox(
-                                height: 20.0,
-                              ),
-                              Image.asset(
-                                "images/pattslogo.png",
-                              ),
-                              SizedBox(
-                                height: 20.0,
-                              ),
-                              Image.asset(
-                                "images/pattslogo.png",
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    ))
-              ],
+                ],
+              )),
             ),
-          )),
+          ),
+          Container(
+            height: 250,
+            color: Colors.red,
+          ),
+          Container(
+            height: 250,
+            color: Colors.yellow,
+          ),
+          Container(
+            height: 250,
+            color: Colors.green,
+          ),
+          Container(
+            height: 250,
+            color: Colors.white,
+          ),
+          Container(
+            height: 250,
+            color: Colors.blue,
+          ),
+        ],
+      ),
+      // body: Container(
+      //     width: double.infinity,
+      //     height: double.infinity,
+      //     child: Padding(
+      //       padding: const EdgeInsets.only(
+      //         top: 50.0,
+      //         left: 20.0,
+      //         right: 20.0,
+      //       ),
+      //       child: Column(
+      //         children: [
+      //           Container(
+      //             child: Row(
+      //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //               children: [
+      // Row(
+      //   children: [
+      //     Text(
+      //       "Welcome, ",
+      //       style: GoogleFonts.poppins(
+      //         fontSize: 25,
+      //         color: const Color.fromARGB(255, 255, 255, 255),
+      //       ),
+      //     ),
+      //     Text(
+      //       "Dreams ",
+      //       style: GoogleFonts.poppins(
+      //           fontSize: 25,
+      //           fontWeight: FontWeight.bold,
+      //           color: Theme.of(context).primaryColor),
+      //     ),
+      //   ],
+      // ),
+      // GestureDetector(
+      //   onTap: () {
+      //     Navigator.push(
+      //         context,
+      //         MaterialPageRoute(
+      //             builder: (context) => MyWelcomeView()));
+      //   },
+      //   child: Container(
+      //     width: 45,
+      //     height: 45,
+      //     decoration: BoxDecoration(
+      //       // color: Colors.white,
+      //       borderRadius: BorderRadius.circular(50),
+      //       border: Border.all(
+      //         width: 3,
+      //         color: Theme.of(context).primaryColor,
+      //       ),
+
+      //       image: DecorationImage(
+      //         image: AssetImage("assets/images/logo2.png"),
+      //         fit: BoxFit.cover,
+      //       ),
+      //     ),
+      //   ),
+      // )
+      //               ],
+      //             ),
+      //           ),
+      //           Row(
+      //             children: [
+      //               Row(
+      //                 children: [
+      //                   Text(
+      //                     "HEADER ",
+      //                     style: GoogleFonts.poppins(
+      //                       fontSize: 25,
+      //                       color: Theme.of(context).primaryColor,
+      //                     ),
+      //                   ),
+      //                   Text(
+      //                     "INFORMATION",
+      //                     style: GoogleFonts.poppins(
+      //                       fontSize: 25,
+      //                       color: Colors.white,
+      //                     ),
+      //                   )
+      //                 ],
+      //               ),
+      //             ],
+      //           ),
+      //           Padding(
+      //             padding: const EdgeInsets.only(top: 8.0),
+      //             child: Row(
+      //               mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //               children: [
+      //                 Text(
+      //                   "News",
+      //                   style: GoogleFonts.poppins(
+      //                     fontSize: 15,
+      //                     color: Colors.white,
+      //                   ),
+      //                 ),
+      //                 Text(
+      //                   "Trending",
+      //                   style: GoogleFonts.poppins(
+      //                     fontSize: 15,
+      //                     color: Colors.white,
+      //                   ),
+      //                 ),
+      //                 Text(
+      //                   "Features",
+      //                   style: GoogleFonts.poppins(
+      //                     fontSize: 15,
+      //                     color: Colors.white,
+      //                   ),
+      //                 ),
+      //                 Text(
+      //                   "Schedule",
+      //                   style: GoogleFonts.poppins(
+      //                     fontSize: 15,
+      //                     color: Colors.white,
+      //                   ),
+      //                 ),
+      //               ],
+      //             ),
+      //           ),
+      //           Container(
+      //               width: double.infinity,
+      //               height: 200,
+      //               child: ListView.builder(
+      //                 scrollDirection: Axis.horizontal,
+      //                 itemBuilder: (BuildContext context, index) {
+      //                   return Padding(
+      //                     padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      //                     child: Column(
+      //                       children: [
+      //                         SizedBox(
+      //                           height: 20.0,
+      //                         ),
+      //                         Image.asset(
+      //                           "images/pattslogo.png",
+      //                         ),
+      //                         SizedBox(
+      //                           height: 20.0,
+      //                         ),
+      //                         Image.asset(
+      //                           "images/pattslogo.png",
+      //                         ),
+      //                         SizedBox(
+      //                           height: 20.0,
+      //                         ),
+      //                         Image.asset(
+      //                           "images/pattslogo.png",
+      //                         ),
+      //                         SizedBox(
+      //                           height: 20.0,
+      //                         ),
+      //                         Image.asset(
+      //                           "images/pattslogo.png",
+      //                         ),
+      //                         SizedBox(
+      //                           height: 20.0,
+      //                         ),
+      //                         Image.asset(
+      //                           "images/pattslogo.png",
+      //                         ),
+      //                         SizedBox(
+      //                           height: 20.0,
+      //                         ),
+      //                         Image.asset(
+      //                           "images/pattslogo.png",
+      //                         ),
+      //                         SizedBox(
+      //                           height: 20.0,
+      //                         ),
+      //                         Image.asset(
+      //                           "images/pattslogo.png",
+      //                         ),
+      //                       ],
+      //                     ),
+      //                   );
+      //                 },
+      //               ))
+      //         ],
+      //       ),
+      //     )),
     );
   }
 }
