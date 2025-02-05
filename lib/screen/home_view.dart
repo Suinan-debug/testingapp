6fe0bf89da85bd83d5e673126beaa98be9aa14b3
Vince Suinan <suinanlawrence8@gmail.com>
2025-02-05 08:46:49 +0800

    Fixed Register form

--- lib/screen/home_view.dart
+++ lib/screen/home_view.dart
@@ -109,7 +109,7 @@ class _HomeViewState extends State<HomeView> {
         ElevatedButton(
           onPressed: () {},
           child: Text(
-            "Register",
+            "Register 1",
             style: TextStyle(fontSize: 20),
           ),
         )
